e84e4ad73da9a604d581f74a2570e90fddf1f28e
Allen Jiang <allenjianggo@gmail.com>
2024-07-05 12:57:50 +0800

    feature: add floating spot animation

--- src/lib/components/sections/Introduction.svelte
+++ src/lib/components/sections/Introduction.svelte
@@ -16,13 +16,73 @@
 		<div class="relative z-0 flex">
 			<img class="z-20 w-[28rem] rounded-full" src={'images/profile.jpg'} alt="Profile" />
 			<div
-				class="absolute right-0 top-0 z-30 size-32 rounded-full border-8 border-gray-50 bg-green-500"
+				class="green-spot-floating absolute right-0 z-30 size-32 rounded-full border-8 border-gray-50 bg-green-500"
 			></div>
-			<div class="absolute bottom-0 right-0 z-10 size-40 rounded-full bg-blue-500"></div>
-			<div class="absolute left-10 top-0 z-10 size-10 rounded-full bg-red-500"></div>
 			<div
-				class="absolute bottom-0 left-16 z-30 size-20 rounded-full border-8 border-gray-50 bg-yellow-500"
+				class="blue-spot-floating absolute bottom-0 right-0 z-10 size-40 rounded-full bg-blue-500"
+			></div>
+			<div class="red-spot-floating absolute left-10 z-10 size-10 rounded-full bg-red-500"></div>
+			<div
+				class="yellow-spot-floating absolute bottom-0 left-16 z-30 size-20 rounded-full border-8 border-gray-50 bg-yellow-500"
 			></div>
 		</div>
 	</div>
 </section>
+
+<style>
+	@keyframes red-spot-floating {
+		0%,
+		100% {
+			top: -1rem;
+		}
+		50% {
+			top: 1rem;
+		}
+	}
+
+	.red-spot-floating {
+		animation: red-spot-floating 4s infinite;
+	}
+
+	@keyframes green-spot-floating {
+		0%,
+		100% {
+			top: -1.5rem;
+		}
+		50% {
+			top: 1.5rem;
+		}
+	}
+
+	.green-spot-floating {
+		animation: green-spot-floating 6s infinite;
+	}
+
+	@keyframes yellow-spot-floating {
+		0%,
+		100% {
+			bottom: 2rem;
+		}
+		50% {
+			bottom: -2rem;
+		}
+	}
+
+	.yellow-spot-floating {
+		animation: yellow-spot-floating 8s infinite;
+	}
+
+	@keyframes blue-spot-floating {
+		0%,
+		100% {
+			bottom: 1rem;
+		}
+		50% {
+			bottom: -2rem;
+		}
+	}
+
+	.blue-spot-floating {
+		animation: blue-spot-floating 10s infinite;
+	}
+</style>
