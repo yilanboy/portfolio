bd81e55bd350bb96af058fe4a942334538541f25
Allen Jiang <allenjianggo@gmail.com>
2024-09-03 14:22:55 +0800

    refactor: update modal transition

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -8,8 +8,7 @@
 {#if showModal}
 	<div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 		<div
-			in:fade
-			out:fade
+			transition:fade
 			id="backdrop"
 			class="fixed inset-0 bg-gray-500 bg-opacity-75 backdrop-blur-md transition-opacity"
 		></div>
@@ -17,8 +16,7 @@
 		<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
 			<div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 				<div
-					in:fade
-					out:fade
+					transition:fade
 					id="modal"
 					class="relative transform overflow-hidden rounded-lg bg-gray-50 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl"
 				>
