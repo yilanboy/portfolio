430b41b5288b6bbdc8d8de5865c62291cfd9b1c7
Allen Jiang <allenjianggo@gmail.com>
2024-09-03 18:43:42 +0800

    refactor: update header

--- src/lib/components/layouts/Header.svelte
+++ src/lib/components/layouts/Header.svelte
@@ -9,13 +9,13 @@
 </script>
 
 <header
-	class={'sticky top-0 z-10 flex items-center justify-between border border-solid px-6 duration-200' +
+	class={'sticky top-2 z-10 mx-2 flex items-center justify-between rounded-md border border-solid px-6 duration-200' +
 		(y > 0 ? ' border-gray-600 bg-gray-50 py-4' : ' border-transparent bg-transparent py-6')}
 >
 	<h1>
 		<b>Allen</b> Jiang
 	</h1>
-	<div class="ml-auto hidden items-center gap-4 pr-4 sm:flex">
+	<div class="ml-auto hidden items-center gap-6 pr-4 sm:flex">
 		{#each tabs as tab (tab.name)}
 			<a href={tab.link} class="hover:text-gray-600">
 				<p>{tab.name}</p>
@@ -25,7 +25,7 @@
 	<a
 		href="https://docfunc.com"
 		target="_blank"
-		class="group relative overflow-hidden rounded-full px-5 py-2 text-slate-950"
+		class="group relative overflow-hidden rounded-full bg-gray-100 px-5 py-2 text-slate-950"
 	>
 		<div
 			class="absolute right-full top-0 z-0 size-full bg-blue-400 opacity-20 duration-200 group-hover:translate-x-full"
