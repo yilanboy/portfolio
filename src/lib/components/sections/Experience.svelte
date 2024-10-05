c6bbd021cc687bbb96b918ef27fca52368d73137
allenjiang <allenjianggo@gmail.com>
2024-10-06 01:20:12 +0800

    refactor: update some style

--- src/lib/components/sections/Experience.svelte
+++ src/lib/components/sections/Experience.svelte
@@ -154,7 +154,7 @@
 		<h3 class="text-3xl font-semibold sm:text-4xl md:text-5xl">
 			{currentTranslation.section_title_prefix}
 			<span
-				class="relative inline-block p-1 before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-yellow-500 dark:before:bg-yellow-600"
+				class="relative inline-block p-1 before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-yellow-400 dark:before:bg-yellow-500"
 			>
 				<span class="relative text-neutral-50">
 					{currentTranslation.section_title_highlight}
@@ -169,7 +169,7 @@
 			<div class="group relative py-6 pl-8 sm:pl-32">
 				<!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
 				<div
-					class="mb-4 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-neutral-300 before:px-px after:absolute after:left-2 after:box-content after:size-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-8 after:border-neutral-50 after:bg-yellow-500 group-last:before:hidden dark:before:bg-neutral-700 dark:after:border-neutral-800 dark:after:bg-yellow-600 sm:flex-row sm:before:left-0 sm:before:ml-[6.5rem] sm:after:left-0 sm:after:ml-[6.5rem]"
+					class="mb-4 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-neutral-300 before:px-px after:absolute after:left-2 after:box-content after:size-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-8 after:border-neutral-50 after:bg-yellow-400 group-last:before:hidden dark:before:bg-neutral-700 dark:after:border-neutral-800 dark:after:bg-yellow-500 sm:flex-row sm:before:left-0 sm:before:ml-[6.5rem] sm:after:left-0 sm:after:ml-[6.5rem]"
 				>
 					<time
 						class="left-0 mb-3 inline-flex h-6 w-20 translate-y-0.5 items-center justify-center rounded-full bg-yellow-200 text-xs font-semibold uppercase text-yellow-700 dark:bg-yellow-600 dark:text-yellow-50 sm:absolute sm:mb-0"
